0435a5a4485f83bf9a0860460871eb98d18126fa
GuilhermeDeSaZampieri <guizampier@gmail.com>
2025-06-05 14:11:36 -0300

    Conectando com api online

--- CurrencyConverter/index.js
+++ CurrencyConverter/index.js
@@ -1,13 +1,47 @@
+const form = document.getElementById("form");
+const from = document.getElementById("from");
+const to = document.getElementById("to");
 const input = document.querySelector("input");
+const p = document.getElementById("result");
 
-if(input.value === text){
+window.addEventListener("load", fetchCurrences);
+form.addEventListener("submit", Result);
 
+async function fetchCurrences() {
+  const resp = await fetch("https://api.exchangerate-api.com/v4/latest/USD");
+  const data = await resp.json();
+
+  const currencyOptions = Object.keys(data.rates);
+  currencyOptions.forEach((currency) => {
+    const option1 = document.createElement("option");
+    option1.value = currency;
+    option1.textContent = currency;
+    from.appendChild(option1);
+
+    const option2 = document.createElement("option");
+    option2.value = currency;
+    option2.textContent = currency;
+    to.appendChild(option2);
+  });
 }
 
-function ClicarButton() {
-    console.log(input.value);
-}
+async function Result(e) {
+  e.preventDefault();
 
-document.querySelector("button").addEventListener("click", () => {
-    ClicarButton();
-});
+  const amount = parseFloat(input.value);
+  const resp = await fetch(
+    `https://api.exchangerate-api.com/v4/latest/${from.value}`
+  );
+  const data = await resp.json();
+  console.log(data);
+
+  if (amount < 0) {
+    alert("Please ener a valid amount");
+    return;
+  }
+
+  const rate = data.rates[to.value];
+  const convert = (rate * amount).toFixed(2);
+
+  p.textContent = `${amount} ${from.value} = ${convert} ${to.value}`;
+}
