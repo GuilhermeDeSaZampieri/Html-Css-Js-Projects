3973a81ced9f04dbcd2b54b1b90c1e3500537b03
GuilhermeDeSaZampieri <guizampier@gmail.com>
2025-06-05 12:13:38 -0300

    Add CurrencyConverter

---
+++ CurrencyConverter/index.js
@@ -0,0 +1,13 @@
+const input = document.querySelector("input");
+
+if(input.value === text){
+
+}
+
+function ClicarButton() {
+    console.log(input.value);
+}
+
+document.querySelector("button").addEventListener("click", () => {
+    ClicarButton();
+});
