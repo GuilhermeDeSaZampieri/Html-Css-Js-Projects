8b606ee8129d0418d5312418bd68469fdc92418d
GuilhermeDeSaZampieri <guizampier@gmail.com>
2025-06-10 21:55:18 -0300

    Finish Password generator

--- PasswordGenerator/script.js
+++ PasswordGenerator/script.js
@@ -62,28 +62,62 @@ const uper = document.getElementById("Uppercase");
 const low = document.getElementById("Lowercase");
 const numb = document.getElementById("Numbers");
 const symb = document.getElementById("Symbols");
-
+const passwordGerate = document.querySelector("p");
 const rangeLen = document.getElementById("lenght");
 const valueRange = document.getElementById("valueRange");
+const copyPass = document.getElementById("copyPass");
+const strong = document.querySelector("strong");
+const loadPassword = document.getElementById("loadPassword");
+const span = document.getElementById("loadingResultPassword")
+
 valueRange.textContent = rangeLen.value;
 
 rangeLen.addEventListener("input", (event) => {
-    valueRange.textContent = event.target.value;
-})
-
+  valueRange.textContent = event.target.value;
+});
 
 const btn = document.querySelector("button");
 btn.addEventListener("click", GeneratePassword);
 
+if (copyPass) {
+  copyPass.addEventListener("click", () => {
+    copyCod(passwordGerate.textContent);
+  });
+}
+
+
 function GeneratePassword(e) {
   e.preventDefault();
+  passwordGerate.textContent = SortedPassword();
 
-  console.log(SortedPassword());
+  if(passwordGerate.textContent.length < 10){
+    strong.style.color = "#963242";
+    strong.textContent = "Weak";
+    span.style.backgroundColor = "#963242";
+    span.style.width = "33%";
+  }
+  else if(passwordGerate.textContent.length >= 10 && passwordGerate.textContent.length <= 20){
+    strong.style.color = "#74add6";
+    strong.textContent = "Medium";
+    span.style.backgroundColor = "#74add6";
+    span.style.width = "66%";
+
+  }
+  else{
+    strong.style.color = "#2c5877";
+    strong.textContent = "Strong";
+    span.style.backgroundColor = "#2c5877";
+    span.style.width = "100%";
+  }
 }
+
 
 function SortedPassword() {
   let password = "";
   const { array, tamanho } = arrayString();
+  if (array.length === 0) {
+    return "Preencha um CheckBox";
+  }
 
   for (let index = 0; index < rangeLen.value; index++) {
     let number = Math.floor(Math.random() * tamanho);
@@ -91,7 +125,6 @@ function SortedPassword() {
   }
   return password;
 }
-
 
 function arrayString() {
   var array = [];
@@ -115,4 +148,16 @@ function arrayString() {
   let tamanho = array.length;
 
   return { array, tamanho };
+}
+
+function copyCod(textInput) {
+  if (textInput) {
+    navigator.clipboard
+      .writeText(textInput)
+      .then(() => window.alert("Copiado com sucesso"));
+  }
+  else{
+    window.alert("Não existe nada");
+  }
+  
 }
